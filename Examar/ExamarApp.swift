c9f987a9372bba30d8f5bc1166040ae8804ef241
Rohan Katyayani <60040641+RohanKatyayani@users.noreply.github.com>
2026-04-07 01:39:52 +0530

    Initial Commit

---
+++ Examar/ExamarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamarApp.swift
+//  Examar
+//
+//  Created by Rohan Katyayani on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ExamarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
